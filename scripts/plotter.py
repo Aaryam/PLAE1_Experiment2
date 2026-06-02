25309dd97a455e18ae3c369ed99d3796ce620548
Aaryam <patnaikaaryan@gmail.com>
2026-06-02 15:59:30 +0200

    Commented stuff:

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -6,8 +6,6 @@ path = os.path.join(os.getcwd(), 'data', 'Day1')
 print(path)
 os.chdir(path)
 
-# path = os.path.join(os.path.pardir(os.getcwd()), 'data', 'Day1', '01062026DataMUO.txt')
-
 fileData = np.loadtxt('01062026DataMUO.txt', dtype='float', delimiter='\t', skiprows=1)
 columnTitles = np.loadtxt('01062026DataMUO.txt', dtype='str', delimiter='\t', max_rows=1)
 
